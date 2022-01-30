dfed10329e609ee990b68df22cf6bda9869f7487
kodokuna_dancer <tyokkanblue58971@icloud.com>
2022-01-31 08:52:46 +0900

    pugをインストール

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,7 @@
-<template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+<template lang="pug">
+  v-app
+    v-main
+      router-view
 </template>
 
 <script lang="ts">
